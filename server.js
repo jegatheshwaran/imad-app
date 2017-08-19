d5192bded23db71aa9df290f656b01833e8991ab
jegatheshwaran <30750119+jegatheshwaran@users.noreply.github.com>
2017-08-19 17:12:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article one', function (req, res) {
-  res.send('article one requested and will be servered here');
+  res.send('article one requested and will be served here');
 });
 
 app.get('/ui/style.css', function (req, res) {
