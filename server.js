c0ad78f3c9849855ba147b8bc9304bd6178808f4
jegatheshwaran <30750119+jegatheshwaran@users.noreply.github.com>
2017-08-21 23:15:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,7 +3,33 @@ var morgan = require('morgan');
 var path = require('path');
 
 var app = express();
-app.use(morgan('combined'));
+app.use(morgan('combined'));<html>
+<head>
+    <title>
+       hi i'm jegatheshwaran: madurai: tamilnadu
+    </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link href="/ui/style.css" rel="stylesheet" />
+    <body>
+    <div class="container">
+        <o hr='/'>Home</o>
+    </div>    
+    <hr/>
+    <h3>
+    Aug 17 in noida
+        </h3>
+   <div>
+      <p>
+    very irritated noida. no genuine person in the world, All are very well selfiesssss 
+    Someone is there with me they are not humans 
+    </p>
+    <p>
+     whose who are cheating others and who cann't care about others, they are respected by world.
+     who are genuine they are mental in the world
+     </p>
+</div>
+</body>    
+</html>
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
