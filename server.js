665eab20f84606afb1fa0f25cf6558b9a6de33d9
jegatheshwaran <30750119+jegatheshwaran@users.noreply.github.com>
2017-08-22 22:25:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,59 +2,6 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 
-var app = express();
-app.use(morgan('combined'));
-<html>
- var ArticleOne= {
-  title:`Article one jegatheshwaran1',
-  heading:`Article one',
-  Date:'22 Aug 2017',
-  content: 
-   `  <p>
-    very irritated noida. no genuine person in the world, All are very well selfiesssss 
-    Someone is there with me they are not humans;
-    </p>
-   
-          <p>
-     whose who are cheating others and who cann't care about others, they are respected by world.
-     who are genuine they are mental in the world;
-     
-          </p>'
-             };
-
-function createTemplate (Data)
-{
-var title=data.title:
-var date= data.date:
-var heading=data.heading:
-var content=data.content:
-
-var htmlTemplate='
-<html>
-<head>
-<title>
-       hi i'm jegatheshwaran: madurai: tamilnadu
- </title>
- <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <link href="/ui/style.css" rel="stylesheet" />
-<body>
- <div class="container">
-<o hr='/'>Home</o>
-</div>    
-   <hr/>
- <h3>
-  $(Heading)         
-</h3>
- $(Date) 
-<div>
- $(content)   
-<</div>
-</body>   
-</html>
-':
-return htmltemplate;
-}
-
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
