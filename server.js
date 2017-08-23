b569cecb9b146ddaaa162bc494e26f39502f3e21
jegatheshwaran <30750119+jegatheshwaran@users.noreply.github.com>
2017-08-23 07:41:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
