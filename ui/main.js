facc075d1588751924b57015bcccf31f44da2ecc
jegatheshwaran <30750119+jegatheshwaran@users.noreply.github.com>
2017-08-28 22:57:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,15 @@
 console.log('Loaded!');
 
+//change the text of main-text div
+
 var element=document.getElementById('main-text');
 element.innerText='New Born';
+
+ //move the image
+ var img=document.getElementbtId('img');
+ img.onclick= function(){
+     img.style.marginleft='100px';
+     
+ };
+ 
+ 
