bc858f02f738e3b4b5fe157feb0b30b48166ade1
jegatheshwaran <30750119+jegatheshwaran@users.noreply.github.com>
2017-08-28 23:12:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,11 +9,11 @@ element.innerText='New Born';
  var img=document.getElementbtId('madi');
  var marginLeft=0;
  function moveRight () {
-     marginLeft =margin+10;
+     marginLeft =marginLeft + 10;
      img.style.marginLeft =marginLeft + 'px';
  }
  img.onclick= function(){
-     var interval=setinterval(moveRight,100);
+     var interval=setinterval (moveRight, 50);
     
     
      
