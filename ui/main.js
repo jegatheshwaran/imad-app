4182aabf17db8a1ff19e079397b90f9445a60a50
jegatheshwaran <30750119+jegatheshwaran@users.noreply.github.com>
2017-08-28 22:59:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,7 @@ var element=document.getElementById('main-text');
 element.innerText='New Born';
 
  //move the image
- var img=document.getElementbtId('img');
+ var img=document.getElementbtId('madi');
  img.onclick= function(){
      img.style.marginleft='100px';
      
