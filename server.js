205bfb8c625cbe1c8dcf55dc6f9ea86c2b1eee07
jegatheshwaran <30750119+jegatheshwaran@users.noreply.github.com>
2017-08-29 22:49:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,6 +94,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter = 0;
+app.get('/counter', function (req, res) {
+  counter = counter+1;
+  res.send(counter.tostring());
+});
+
+
+
+
 app.get('/:articlename', function (req, res) {
     //articleone ==article-one
     //article[articlename]={} content for article one
