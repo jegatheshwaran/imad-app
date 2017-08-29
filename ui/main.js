804a935944cdaddfb91c0308067fbcd46dfaea9f
jegatheshwaran <30750119+jegatheshwaran@users.noreply.github.com>
2017-08-29 22:55:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ console.log('Loaded!');
 //change the text of main-text div
 
 var element=document.getElementById('main-text');
-element.innerText='New Born';
+element.innerText = 'New Born';
 
  //move the image
  var img=document.getElementById('madi');
