d9d213a0dd20bb7569fc600c7ca201916443dc85
jegatheshwaran <30750119+jegatheshwaran@users.noreply.github.com>
2017-09-02 12:56:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,34 +3,32 @@
  
  button.onclick = function(){
      
-   //create a request object
+  
+   {
+             var counter = request.responeText;
+             var span = document.getElementById('counter');
+             span.innerHTML = counter.tostring();
+        
+         }
+       }
+       // no done yet
+   
+
+ 
+   //submit name
+    var nameinput= document.getElementById('name');
+    var names = nameinput.value;
+    var submit = document.getElementById('submit_btn');
+    submit.onclick = function(){
+         //create a request object
    var request = new XMLHttpRequest();
    
     //capture the respone and store is in variable
    request.onReadystatechange =function(){
        if (request.readystate === XMLHttpRequest.DONE){
         //take some action 
-         if (request.status === 200){
-             var counter = request.responeText;
-             var span = document.getElementById('counter');
-             span.innerHTML = counter.tostring();
-         
-         }
-       }
-       // no done yet
-   };
-   
-   //make the request
-   request.open('GET', 'http://jegatheshwaran36.imad.hasura-app.io/counter',true);
-   request.send(null);
- };
- 
-   //submit name
-    var nameinput= document.getElementById('name');
-    var name = nameinput.value;
-    var submit = document.getElementById('submit_btn');
-    submit.onclick = function(){
-        //make the request to server and send the name
+         if (request.status === 200)
+         {
         //capture a list of  the name and render as it as a list
         var names =['name1','name2','name3','name4'];
         var list='';
@@ -40,7 +38,12 @@
         }
         var ul= document.getElementById('namelist');
         ul.innerHTML=list;
-    };
+    }
+       }
+       //yet not done
  };
-   
+      //make the request
+   request.open('GET', 'http://jegatheshwaran36.imad.hasura-app.io/counter',true);
+   request.send(null);
+ };
  
