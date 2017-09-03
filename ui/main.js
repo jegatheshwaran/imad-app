d3ffc40c1f3fa86f88fa6183122e3ae2c3e872ce
jegatheshwaran <30750119+jegatheshwaran@users.noreply.github.com>
2017-09-03 12:46:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,38 +23,39 @@
    request.send(null); 
    };
  
-   //submit name
-    var nameinput= document.getElementById('name');
-    var names = nameinput.value;
-    var submit = document.getElementById('submit_btn');
-    submit.onclick = function(){
-         //create the response and store it in a vaeiable
-  request.onreadystatechange=function(){
-      
-      if (request.readystate===XTMLHttpRequest.Done){
-          //take action 
-          if (requset. status === 200)
-   {    
-       //capture a list of  the name and render as it as a list
-        var names = request.responseText;
-        names =JSON.parse(names);
-        var list='';
-        for (var i=0; i<names.length; i++){
-            list += '<li>' +names[i]+ '</li>';
-            
+
+//Submit username and password
+var submit = document.getElementById('submit_btn');
+submit.onclick = function(){
+    //Create a request object
+    var request = new XMLHttpRequest();
+
+    //Capture the response and store it in a variable
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            if (request.status === 200){
+                console.log('user logged in');
+                alert('log in success');
+            } else if (request.status === 403 ){
+                alert('invalid user')
+            } else if (request.status === 500){
+                alert('Something is wrong with the server');
+            }
         }
-        var ul= document.getElementById('namelist');
-        ul.innerHTML=list;
-  
-         }
-       }
-       // no done yet
-  };
- //make the request
-   request.open('GET', 'http://jegatheshwaran36.imad.hasura-app.io/submit-names=' +name ,true);
-   request.send(null); 
-        };
+    };
         
-   
-  
- 
+    //Submit comment
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    //enable for debugging only
+    //console.log(username);
+    //console.log(password);
+
+    //Make a request to the counter. use below if running on hasura-app
+    //request.open('GET', 'http://achy007mails.imad.hasura-app.io/submit-comment?comment=' + comment, true);
+
+    //Make a request, use below if running on localhost
+    request.open('POST', 'http://../login', true);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.send(JSON.stringify({username: username, password: password}));    
+};
