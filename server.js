6c98be03afbe1cc7aec5f0f4f741710f19444177
jegatheshwaran <30750119+jegatheshwaran@users.noreply.github.com>
2017-09-12 00:13:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var pool = require('pg').pool;
 var crypto = require('crypto');
-var bodyparser = require('body-parser');
+var bodyParser = require('body-parser');
 
 var config = {
     user:'jegatheshwaran36',
@@ -16,7 +16,7 @@ var config = {
 
 var app=express();
 app.use(morgan('combined'));
-app.use(bodyparser.JSON());
+app.use(bodyParser.JSON());
 
 
 function createTemplate(data){
