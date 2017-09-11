fe8e997aa0e4e99d41892956e7eaa08598d916ff
jegatheshwaran <30750119+jegatheshwaran@users.noreply.github.com>
2017-09-12 00:03:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var pool = require('pg').pool;
 var crypto = require('crypto');
+var bodyparser = require('bodyparser');
 
 var config = {
     user:'jegatheshwaran36',
@@ -15,6 +16,7 @@ var config = {
 
 var app=express();
 app.use(morgan('combined'));
+app.use(bodyparser.JSON();
 
 
 function createTemplate(data){
@@ -66,6 +68,24 @@ function hash(input,salt){
 app.get('/hash/:input', function (req, res) {
     var hashedstring = hash (req.params.input,'this-is-some-random-string');
   res.send(hashedstring);
+});
+
+app.post('create-aser',function(req,res)){
+    //username,password
+    //JSON
+    var username = req.body.username;
+    var password = req.body.password;
+    var salt = crypto.getRandomBytes(128).tostring('hex');
+    dbstring = hash (password,salt);
+    pool.query('INSERT INTO "user"(username,password)values($1,$2)',[username,dbstring],function(err,result){
+        if (err){
+            res.status(500).send(err.tostring());
+        }
+        else{
+            res.send('user successfully created:' + username);
+        }
+    }
+    });
 });
 
 var Pool=require('pg').Pool;
