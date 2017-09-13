05919234e44a7379f97d8dd730e6032708b2fa88
jegatheshwaran <30750119+jegatheshwaran@users.noreply.github.com>
2017-09-13 23:48:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,6 @@
 
 //Submit username and password to login
-var NamesInput = document.getElementById('names');
-var Names= namesInput.value;
+
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
     //Create a request object
@@ -12,10 +11,10 @@ submit.onclick = function(){
         if(request.readyState === XMLHttpRequest.DONE){
             //take sone action
             if (request.status === 200){
-                console.log('user logged in');
+            
                 alert('log in success');
             } else if (request.status === 403 ){
-                alert('invalid user')
+                alert('invalid user');
             } else if (request.status === 500){
                 alert('Something is wrong with the server');
             }
@@ -23,17 +22,12 @@ submit.onclick = function(){
         // not done yet
     };
         
-    //Submit comment
-    var username = document.getElementById('username').value;
+
+      //Make a request
+      var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
-    //enable for debugging only
-    //console.log(username);
-    //console.log(password);
-
-    //Make a request to the counter. use below if running on hasura-app
-    //request.open('GET', "http://jegatheshwaran36.imad.hasura-app.io/submit-comment?comment="" + comment, true);
-
-    //Make a request, use below if running on localhost
+    console.log(username);
+    console.log(password);
     request.open('POST', 'http://jegatheshwaran36.imad.hasura-app.io/login', true);
     request.setRequestHeader('Content-Type', 'application/json');
     request.send(JSON.stringify({username: username, password: password}));    
