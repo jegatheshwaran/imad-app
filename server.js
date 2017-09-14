3618f87d9611d638b0809b48f5e90db4ad91083c
jegatheshwaran <30750119+jegatheshwaran@users.noreply.github.com>
2017-09-14 23:41:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var pool = require('pg').pool;
 var crypto = require('crypto');
-var bodyParser = require('body-parser');
+var bodyParser = require('body-Parser');
 var session = require('express-session');
 
 var config = {
